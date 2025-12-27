a04130a25caef2dccfc4d5f7b2b8bfb51b304800
apurvwaghmare <apurvawaghamare1234@gmail.com>
2025-12-27 22:42:20 +0530

    added shoe image to learn props

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,35 +1,25 @@
 import React from "react";
 import "./App.css";
-// import Header from "./Components/Header";
-// import Hero from "./Components/Hero";
-// import Footer from "./Components/Footer";
-import Child from "./Components/Child";
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+import Footer from "./Components/Footer";
+import shoes from "./Assets/shoes.png";
+import shoe2 from "./Assets/shoe2.png";
+import shoe3 from "./Assets/shoe3.png";
+import shoe4 from "./Assets/shoe4.png";
+import shoe5 from "./Assets/shoe5.png";
 const App = () => {
     return (
         <div className="app">
-
-        <Child name="brother!" name2="you!"/>
-    
-
-
-
-
-
-
-
-
-
-
-
-            {/* <Header/>
+            <Header/>
             <div className = "shoe">
-                <Hero/>
-                <Hero/>
-                <Hero/>
-                <Hero/>
-                <Hero/>
+                <Hero title="Blktop Rider Suede Sneakers" price="8099" img={shoes}/>
+                <Hero title="Speedcat OG Sneakers" price="9999" img={shoe2}/>
+                <Hero title="Palermo Classic Sneakers" price="5249" img={shoe3}/>
+                <Hero title="Speedcat OG Sneakers" price="9999" img={shoe4}/>
+                <Hero title="Ascenso Sneakers" price="6499" img={shoe5}/>
             </div>
-            <Footer/> */}
+            <Footer/>
 
 
         </div>
--- react-course/src/Components/Hero.js
+++ react-course/src/Components/Hero.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./Hero.css";
-import shoes from "../Assets/shoes.png";
-const Hero = () => {
+// import shoes from "../Assets/shoes.png";
+// import shoe2 from "../Assets/shoe2.png";
+// import shoe3 from "../Assets/shoe3.png";
+// import shoe4 from "../Assets/shoe4.png";
+// import shoe5 from "../Assets/shoe5.png";
+const Hero = ({title, price, img}) => {
     return (
         <div className = "hero">
             <h2 style={{textAlign:"center"}}>TRENDING NOW</h2>
             <div style={{border:"2px solid gray", width:"250px", padding:"5px 5px"}}>
                 <div>
-                    <img src={shoes} alt="shoes" style={{ marginLeft: "20px" }} />
+                    <img src={img} alt="shoes" style={{ marginLeft: "20px" }} />
                 </div>
                 <div className = "shoes" style={{display:"flex"}}>
-                    <p>Blktop Rider Suede Sneakers</p>
-                    <p>₹8,099</p>
+                    <p>{title}</p>
+                    <p>{price}</p>
                 </div>
             </div>
         </div>
