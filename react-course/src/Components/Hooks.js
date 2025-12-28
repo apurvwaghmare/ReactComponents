ded6c987e1448789eb4527003930764ba9e5e4c6
apurvwaghmare <apurvawaghamare1234@gmail.com>
2025-12-28 12:24:25 +0530

    added change name using useState

--- react-course/src/Components/Hooks.js
+++ react-course/src/Components/Hooks.js
@@ -28,10 +28,17 @@ const Hooks = () => {
         setCount(prev => prev + 1);
     };
 
+    const [name, setName] = useState("apurv");
+    const changeName= () => {
+        setName(prev => prev === "apurv" ? "asmi" : "apurv");
+    };
+
     return (
         <div>
+            <h1>{name}</h1>
             <p>number {counter}</p>
             <button onClick={handlePlus}>add</button>
+            <button onClick={changeName}>changeName</button>
         </div>
     );
 };
