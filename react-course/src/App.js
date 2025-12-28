b7f9ab747e0b9c529923fe1348317d0bbbaaf6e3
apurvwaghmare <apurvawaghamare1234@gmail.com>
2025-12-28 09:36:49 +0530

    learned how to get props through array

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -9,15 +9,46 @@ import shoe3 from "./Assets/shoe3.png";
 import shoe4 from "./Assets/shoe4.png";
 import shoe5 from "./Assets/shoe5.png";
 const App = () => {
+
+    // const data = [{title:"Blktop Rider Suede Sneakers", price:"8099", img:shoes},
+    //             {title:"Speedcat OG Sneakers", price:"9999", img:shoe2},
+    //             {title:"Palermo Classic Sneakers", price:"5249", img:shoe3},
+    //             {title:"Speedcat OG Sneakers", price:"9999", img:shoe4},
+    //             {title:"Ascenso Sneakers", price:"6499", img:shoe5}
+    // ];
+
+    // return (
+    //     <div className="app">
+    //         <Header/>
+    //         <div className = "shoe">
+    //             <Hero title={data[0].title} price={data[0].price} img={data[0].img}/>
+    //             <Hero title={data[1].title} price={data[1].price} img={data[1].img}/>
+    //             <Hero title={data[2].title} price={data[2].price} img={data[2].img}/>
+    //             <Hero title={data[3].title} price={data[3].price} img={data[3].img}/>
+    //             <Hero title={data[4].title} price={data[4].price} img={data[4].img}/>
+    //         </div>
+    //         <Footer/>
+
+
+    //     </div>
+    // );
+
+        const data = [{title:"Blktop Rider Suede Sneakers", price:"8099", img:shoes},
+                {title:"Speedcat OG Sneakers", price:"9999", img:shoe2},
+                {title:"Palermo Classic Sneakers", price:"5249", img:shoe3},
+                {title:"Speedcat OG Sneakers", price:"9999", img:shoe4},
+                {title:"Ascenso Sneakers", price:"6499", img:shoe5}
+    ];
+
     return (
         <div className="app">
             <Header/>
             <div className = "shoe">
-                <Hero title="Blktop Rider Suede Sneakers" price="8099" img={shoes}/>
-                <Hero title="Speedcat OG Sneakers" price="9999" img={shoe2}/>
-                <Hero title="Palermo Classic Sneakers" price="5249" img={shoe3}/>
-                <Hero title="Speedcat OG Sneakers" price="9999" img={shoe4}/>
-                <Hero title="Ascenso Sneakers" price="6499" img={shoe5}/>
+                <Hero title={data[0].title} price={data[0].price} img={data[0].img}/>
+                <Hero title={data[1].title} price={data[1].price} img={data[1].img}/>
+                <Hero title={data[2].title} price={data[2].price} img={data[2].img}/>
+                <Hero title={data[3].title} price={data[3].price} img={data[3].img}/>
+                <Hero title={data[4].title} price={data[4].price} img={data[4].img}/>
             </div>
             <Footer/>
 
