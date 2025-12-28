13237cc95639aa2cc5fa2ec0371fdb10c79423fc
apurvwaghmare <apurvawaghamare1234@gmail.com>
2025-12-28 10:17:20 +0530

    map() function

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -33,26 +33,31 @@ const App = () => {
     //     </div>
     // );
 
-        const data = [{title:"Blktop Rider Suede Sneakers", price:"8099", img:shoes},
-                {title:"Speedcat OG Sneakers", price:"9999", img:shoe2},
-                {title:"Palermo Classic Sneakers", price:"5249", img:shoe3},
-                {title:"Speedcat OG Sneakers", price:"9999", img:shoe4},
-                {title:"Ascenso Sneakers", price:"6499", img:shoe5}
+    const data = [
+        { id: 1, title: "Blktop Rider Suede Sneakers", price: "8099", img: shoes },
+        { id: 2, title: "Speedcat OG Sneakers", price: "9999", img: shoe2 },
+        { id: 3, title: "Palermo Classic Sneakers", price: "5249", img: shoe3 },
+        { id: 4, title: "Speedcat OG Sneakers", price: "9999", img: shoe4 },
+        { id: 5, title: "Ascenso Sneakers", price: "6499", img: shoe5 }
     ];
+
 
     return (
         <div className="app">
             <Header/>
             <div className = "shoe">
-                <Hero title={data[0].title} price={data[0].price} img={data[0].img}/>
-                <Hero title={data[1].title} price={data[1].price} img={data[1].img}/>
-                <Hero title={data[2].title} price={data[2].price} img={data[2].img}/>
-                <Hero title={data[3].title} price={data[3].price} img={data[3].img}/>
-                <Hero title={data[4].title} price={data[4].price} img={data[4].img}/>
+
+                {/* using map function  */}
+                {
+                    data.map((item) => {
+                        return (
+                            <Hero key={item.id} title={item.title} price={item.price} img={item.img}/>
+                        );
+                    })
+                }
+
             </div>
             <Footer/>
-
-
         </div>
     );
 };
